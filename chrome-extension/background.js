4d7b71a0983ff2e176e24ebc1edd6a790781b6d4
Beck <iz596192@ucf.edu>
2024-07-15 16:51:24 -0400

    Update chrome-extension/

---
+++ chrome-extension/background.js
@@ -0,0 +1,8 @@
+chrome.tabs.onCreated.addListener(tab => {
+    // Logic to handle new tab creation
+  });
+  
+  chrome.tabs.onRemoved.addListener(tabId => {
+    // Logic to handle tab removal
+  });
+  
